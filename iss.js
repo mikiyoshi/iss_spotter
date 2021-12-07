3b1338c5e3cb7af68f5483ceaf4a2a7e25083e5d
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-12-07 05:20:06 +0000

    this file updated for new files

--- iss.js
+++ iss.js
@@ -1,28 +1,40 @@
-const { callbackify } = require('util');
+// iss.js
 
-const fetchMyIP = function (callback) {
-  const request = require('request');
+const request = require('request');
 
-  const domain = 'https://api64.ipify.org?format=json';
+// ... other three functions not included in solution ...
 
-  request(domain, (error, response, body) => {
+/**
+ * Orchestrates multiple API requests in order to determine the next 5 upcoming ISS fly overs for the user's current location.
+ * Input:
+ *   - A callback with an error or results.
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly-over times as an array (null if error):
+ *     [ { risetime: <number>, duration: <number> }, ... ]
+ */
+const nextISSTimesForMyLocation = function (callback) {
+  fetchMyIP((error, ip) => {
     if (error) {
-      // console.log('Request Failed');
-      callback(error, null);
-      return;
-    }
-    // console.log(data.ip);
-    if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
-      return;
+      return callback(error, null);
     }
 
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
+    fetchCoordsByIP(ip, (error, loc) => {
+      if (error) {
+        return callback(error, null);
+      }
+
+      fetchISSFlyOverTimes(loc, (error, nextPasses) => {
+        if (error) {
+          return callback(error, null);
+        }
+
+        callback(null, nextPasses);
+      });
+    });
   });
 };
 
-module.exports = {
-  fetchMyIP,
-};
+// Only export nextISSTimesForMyLocation and not the other three (API request) functions.
+// This is because they are not needed by external modules.
+module.exports = { nextISSTimesForMyLocation };
